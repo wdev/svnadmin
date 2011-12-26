5dba2b07a7daad789e9137af0f1d98f726c82ee2
Gustavo Henrique <gustavo@gustavohenrique.net>
2011-12-26 15:36:17 -0200

    added automatic deploy in build.sh

--- build.sh
+++ build.sh
@@ -3,35 +3,69 @@ set xe
 PLAY="/opt/play/play"
 OUTPUT_WAR_DIR="war/svnadmin"
 
-$PLAY deps
-$PLAY auto-test
-
-cd test-result
-(
-echo "<html><head><title>Test results</title></head><body><ul>"
-
-for i in *.failed.html ; do
-    if [ -e $i ] ; then
-        echo "<li><a href="$i">$i</li>"
-    fi
-done
-
-echo "</ul><p><ul>"
-
-for i in *.passed.html ; do
-    if [ -e $i ] ; then
-        echo "<li><a href="$i">$i</li>"
-    fi
-done
-
-echo "</ul></body></html>"
-) > index.html
-
-if [ -e result.failed ] ; then
-    exit 1
-else
-    cd ..
-    $PLAY war . -o $OUTPUT_WAR_DIR --exclude .svn:war:test-result:tmp:eclipse:logs --%prod --zip
+RELEASE=`cat RELEASE 2> /dev/null`
+if [ "$RELEASE" == "" ]; then
+    RELEASE=1
+    echo $RELEASE > RELEASE
 fi
-cd -
+
+WAR_FILE="svnadmin##0$RELEASE.war"
+
+function test() {
+    $PLAY auto-test
+
+    cd test-result
+    (
+    echo "<html><head><title>Test results</title></head><body><ul>"
+
+    for i in *.failed.html ; do
+        if [ -e $i ] ; then
+            echo "<li><a href="$i">$i</li>"
+        fi
+    done
+
+    echo "</ul><p><ul>"
+
+    for i in *.passed.html ; do
+        if [ -e $i ] ; then
+            echo "<li><a href="$i">$i</li>"
+        fi
+    done
+
+    echo "</ul></body></html>"
+    ) > index.html
+    
+    if [ -e result.failed ] ; then
+        exit 1
+    fi
+    
+    cd ..
+}
+
+function create_war() {
+    rm -rf war 2> /dev/null
+    
+    $PLAY war . -o $OUTPUT_WAR_DIR --exclude .svn:.git:war:test-result:tmp:eclipse:logs:application.log --%prod
+
+    # Add tomcat basic auth in web.xml
+    cp conf/tomcat-basic-auth-web.xml $OUTPUT_WAR_DIR/WEB-INF/web.xml
+    
+    # Create war file
+    cd $OUTPUT_WAR_DIR
+    zip ../$WAR_FILE -1 -r .
+    cd -
+    
+    # Increment release number
+    expr $RELEASE + 1 > RELEASE
+}
+
+function deploy() {
+    scp war/$WAR_FILE ips@icbradesco:/opt/ips/tomcat7/webapps/
+}
+
+
+git pull
+#test
+create_war
+#deploy
 
